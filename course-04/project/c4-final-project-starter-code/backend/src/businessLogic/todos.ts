158e9f9318fedfc84c061f08f6e23b7628d0f9cd
Emiya Shiro <tuquochung123@gmail.com>
2023-06-16 10:55:30 +0700

    Completed implementation Update, delete, upload image feature.

--- course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
@@ -1,16 +1,16 @@
 import { TodosAccess } from '../dataLayer/todosAcess'
 import { AttachmentUtils } from '../helpers/attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
-// import { TodoUpdate } from '../models/TodoUpdate'
+import { TodoUpdate } from '../models/TodoUpdate'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 
 //TODO: Implement businessLogic
 const logger = createLogger('TodosAcess')
 const attachmentUtils = new AttachmentUtils()
-const todosAcess = new TodosAccess()
+const todosAccess = new TodosAccess()
 
 /**
  * name: fnCreateTodo
@@ -36,7 +36,7 @@ export async function fnCreateTodo(
     ...newTodo
   }
 
-  return await todosAcess.fnCreateTodoItem(newItem)
+  return await todosAccess.fnCreateTodoItem(newItem)
 }
 
 /**
@@ -48,5 +48,49 @@ export async function fnCreateTodo(
 export async function fnGetTodosForUser(userId: string): Promise<TodoItem[]> {
   logger.info('Get todos for user function called.')
 
-  return todosAcess.fnGetAllTodos(userId)
+  return todosAccess.fnGetAllTodos(userId)
+}
+
+/**
+ * name: fnUpdateTodo
+ * @param: userId, todoId, todoUpdate
+ * @return {todosAccess} A promise
+ * description: Update data of todo task
+ */
+export async function fnUpdateTodo(
+  userId: string,
+  todoId: string,
+  todoUpdate: UpdateTodoRequest
+): Promise<TodoUpdate> {
+  logger.info('Update todo function called')
+
+  return await todosAccess.fnUpdateTodoItem(userId, todoId, todoUpdate)
+}
+
+/**
+ * name: fnDeleteTodo
+ * @param: userId, todoId
+ * @return {todosAccess} A promise
+ * description: Delete todo item
+ */
+export async function fnDeleteTodo(
+  userId: string,
+  todoId: string
+): Promise<string> {
+  logger.info('Delete todo function called')
+
+  return todosAccess.fnDeleteTodoItem(userId, todoId)
+}
+
+/**
+ * name: fnCreateAttachmentPresignedUrl
+ * @param: todoId
+ * @return {todosAccess} A promise
+ * description: Upload image
+ */
+export async function fnCreateAttachmentPresignedUrl(
+  todoId: string
+): Promise<string> {
+  logger.info('Create attachment function called', todoId)
+  return attachmentUtils.fnGetUploadUrl(todoId)
 }
